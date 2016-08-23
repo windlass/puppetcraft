b55dd95d3c254933da84bda075fba48eb5507f02
Ubuntu <ubuntu@ip-172-31-13-209.us-west-1.compute.internal>
2016-08-23 21:04:44 +0000

    barebones apache and php5 modules

--- puppet/modules/apache/manifests/init.pp
+++ puppet/modules/apache/manifests/init.pp
@@ -1,9 +1,12 @@
+class apache {
+
 # install apache2 package
 package { 'apache2':
   ensure => installed,
-}
+   }
 
 # ensure apache2 service is running
 service { 'apache2':
   ensure => running,
+   }
 }
--- puppet/modules/php5/manifests/init.pp
+++ puppet/modules/php5/manifests/init.pp
@@ -1,3 +1,6 @@
+include ::apache
+
+class php5 {
 # install php5 package
 package { 'php5':
   ensure => installed,
@@ -7,5 +10,6 @@ package { 'php5':
 file { '/var/www/html/info.php':
   ensure => file,
   content => '<?php  phpinfo(); ?>',    # phpinfo code
-  require => Package[apache::'apache2'],        # require 'apache2' package before creating
+  require => Package['apache2'],        # require 'apache2' package before creating
+}
 }
